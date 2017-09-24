dc7274cc81765b62764a17c3931a526682747aca
Nicholas Vanhaute <njvanhaute@crimson.ua.edu>
2017-09-24 01:21:37 -0500

    Graphs and IBM response processing

--- routes/emotion.js
+++ routes/emotion.js
@@ -2,28 +2,41 @@ var plotly = require('plotly')("njvanhaute", "ea4yKWKZ9ELfBXpLDHXz");
 var express = require('express');
 var router = express.Router();
 
-var angerFloat = 0.68;
-var digustFloat = 0.42;
-var fearFloat = 0.32;
-var joyFloat = .80;
-var sadnessFloat = .2;
+var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+var tone_analyzer = new ToneAnalyzerV3({
+  username: 'c5028d38-e106-4b76-b284-00db3c700cf8',
+  password: 'xBpEgD8AgOsO',
+  version_date: '2017-09-18'
+});
+// Send request with Joey text
+//reponse = JSON object returned by IBM api
+var response = x;
+var responseValues = [];
+var nameVariables = [];
+for (var i = 0; i < 5; i++)
+{
+  responseValues[i] = response['document_tone']['tone_categories']['tones'][i];
+  nameVariables[i] = responseValues[i]['tone_name'];
+  
+}
 
 var data = [
   {
-    x: ["giraffes", "orangutans", "monkeys"],
-    y: [20, 14, 23],
+    x: nameVariables,
+    y: responseValues,
+    marker: {color: ["#FF3601", "#E88000", "#FFCB00", "#CDE800", "#1DFF0D"]},
     type: "bar"
   }
 ];
-
+//
 var graphOptions = {filename: "basic-bar", fileopt: "overwrite"};
 plotly.plot(data, graphOptions, function(err, msg) {
   console.log(msg);
 });
 
 router.get('/', function(req, res, next) {
-    res.render('emotion', { title2: 'Stats', anger: angerFloat, digust: digustFloat, fear: fearFloat, joy: joyFloat, sadness: sadnessFloat });
+    res.render('emotion', { title2: 'Stats', anger: responseValues[0], digust: responseValues[1], fear: responseValues[2], joy: responseValues[3], sadness: responseValues[4]});
 });
 
 module.exports = router;
-// Comment, Hey
+// Watson API key: Gk8RNv8sb60vinrgJ9xbA87iE3tD5cd-ihN6CiavG2Pu
