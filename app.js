896b7491117a286e00f8132403efa2f4f9f42572
jmurp7385 <jmurp7385@gmail.com>
2017-09-24 10:27:41 -0500

    demo ready

--- app.js
+++ app.js
@@ -12,17 +12,16 @@ if (process.env.NODE_ENV !== 'production') {
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-// var emotion = require('./routes/emotion');
-// var search = require('./routes/twitter_search');
 
 var app = express();
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,8 +30,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-// app.use('/', emotion);
-// app.use('/search', search);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
