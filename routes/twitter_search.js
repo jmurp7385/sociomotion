b93339069c784d16abf38cfa2f49cf31cc36c444
jmurp7385 <jmurp7385@gmail.com>
2017-09-23 21:39:24 -0500

    updates

--- routes/twitter_search.js
+++ routes/twitter_search.js
@@ -1,22 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var Twitter = require('twitter');
 
 /* GET home page. */
 router.get('/search', function(req, res, next) {
   res.render('index', { title: 'Seach a user' });
-  var twitterAPI = require('node-twitter-api');
-  var twitter = new twitterAPI({
-      consumerKey: 'GyeiEZokW8JWnP5Hdh7EOQtYO',
-      consumerSecret: 'BFhddugDswIaDPBcSjvMuPRBGYtoZa4pckqVx7yi5ZUebpC341',
-      // callback: 'http://yoururl.tld/something'
+   
+  var client = new Twitter({
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: ''
   });
-  twitter.getRequestToken(function(error, requestToken, requestTokenSecret, results){
-      if (error) {
-          console.log("Error getting OAuth request token : " + error);
-      } else {      
-          //store token and tokenSecret somewhere, you'll need them later; redirect user       
-          app.use(session({ request: requestToken, request_secret: requestTokenSecret, cookie: { maxAge: 60000 }}));
-      }
+   
+  var params = {screen_name: 'nodejs'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+      console.log(tweets);
+    }
   });
 });
 
